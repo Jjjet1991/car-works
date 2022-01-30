06bbfd51362271cae1d309cbc9074e73fe8601d4
Jourdan Neal <jjjet1991@gmail.com>
2022-01-29 23:52:55 -0800

    cleaning up files

---
+++ js/footer.js
@@ -0,0 +1,23 @@
+class Footer extends HTMLElement {
+    constructor(){
+        super();
+    }
+
+ connectedCallback(){
+     this.innerHTML = `
+     <footer>
+  <div style="background-color: black;width:100%;height:170px;">
+      <div class="contact-info">
+      <p style="font-size:20px; text-decoration:underline;">Car Works</p>
+      <p class="contact">421 S. Dishman Mica Rd</p>
+      <p class="contact">(509) 927-8440</p>
+      <p class="contact"><em>carworksspokane@gmail.com</em></p>
+      <a href="https://www.facebook.com/Car-Works-1540509986234813/"><i class="fa fa-facebook-official fa-2x" aria-hidden="true" style="color:gray"></i></a>
+     </div>
+  </div>
+</footer>
+     `
+ }
+}
+
+customElements.define('footer-component', Footer);
---
+++ js/header.js
@@ -0,0 +1,34 @@
+class Header extends HTMLElement
+{
+constructor() {
+super();
+}
+
+connectedCallback() {
+    this.innerHTML=`
+        
+    <nav class="navbar navbar-expand-lg navbar-light">
+    <a href="#" class="navbar-brand logo"><img src="./images/carWorksLogo.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon" style="color:white;background-color: white;"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <ul class="navbar-nav mr-auto mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" style="color:white;font-size:22px;font-family: 'Space Grotesk', sans-serif;" href="index.html">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"style="color:white;font-size:22px;font-family: 'Space Grotesk', sans-serif;" href="maintenance.html">Maintenance</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="color:white;font-size:22px;font-family: 'Space Grotesk', sans-serif;" href="repair.html">Repairs</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="color:white;font-size:22px;font-family: 'Space Grotesk', sans-serif;" href="contact.html">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>`;
+  }
+}
+customElements.define('header-component', Header);
