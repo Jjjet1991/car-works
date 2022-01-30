39a457a577e8dfeda34b73e1353da636ef0b88e4
Jourdan Neal <jjjet1991@gmail.com>
2022-01-30 02:29:50 -0800

    fixing hero image and fb link

--- js/footer.js
+++ js/footer.js
@@ -12,7 +12,7 @@ class Footer extends HTMLElement {
       <p class="contact">421 S. Dishman Mica Rd</p>
       <p class="contact">(509) 927-8440</p>
       <p class="contact"><em>carworksspokane@gmail.com</em></p>
-      <a href="https://www.facebook.com/Car-Works-1540509986234813/"><i class="fa fa-facebook-official fa-2x" aria-hidden="true" style="color:gray"></i></a>
+      <a href="https://www.facebook.com/Car-Works-1540509986234813/" target="blank";><i class="fa fa-facebook-official fa-2x" aria-hidden="true" style="color:gray"></i></a>
      </div>
   </div>
 </footer>
